f01c7e15b6e0b754d9e3281e1ad1e58232e9ef8b
Pablo Barrales <psbarrales@gmail.com>
2022-02-04 20:48:31 -0300

    test(config): fix configuration

--- __tests__/pages/index.test.js
+++ __tests__/pages/index.test.js
@@ -1,13 +1,11 @@
 import { render, screen } from '@testing-library/react'
-import HomePage from 'src/pages/index.js'
+import HomePage from 'pages/index.js'
 
 describe('HomePage', () => {
   it('renders a heading', () => {
     render(<HomePage />)
 
-    const heading = screen.getByRole('heading', {
-      name: /home page/i,
-    })
+    const heading = screen.getByText(/home page/i)
 
     expect(heading).toBeInTheDocument()
   })
